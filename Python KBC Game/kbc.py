05fe6fb94b681996682ce4258c3b47d9cec646f8
Anurag Rawat <41102131+anuragrawat19@users.noreply.github.com>
2019-05-14 11:04:04 +0530

    Update kbc.py

--- Python KBC Game/kbc.py
+++ Python KBC Game/kbc.py
@@ -1,3 +1,5 @@
+#a list name "question stores all the 15 question of KBC game"
+
 questions = ["1. Former Australian captain Mark Taylor has had several nicknames over his playing career. Which of the following was NOT one of them", 
 "2 . Which was the 1st non Test playing country to beat India in an international match" , 
 "3 . Track and field star Carl Lewis won how many gold medals at the 1984 Olympic games" ,
@@ -13,40 +15,45 @@ questions = ["1. Former Australian captain Mark Taylor has had several nicknames
  "13. The ratio of width of our National flag to its length is",
  "14. Rabindranath Tagore's 'Jana Gana Mana' has been adopted as India's National Anthem. How many stanzas of the said song were adopted",
  "15. Natya - Shastra the main source of India's classical dances was written by"]
+
+
+#"first_options" list contains all the first options of all the question and so the others list too like second_options,third_options and fourth_options.
+
 first_options = ["0. Tubby" , "0. Canada" , "0. Two" , "0. Leicestershire" , "0. Geet Sethi" , "0. M.L.Valsamma" , "0. Northamptonshire & Worcestershire" , "0. 1936" , "0. Cactus Jack" , "0. The Rickster" , "0. 45 seconds" ,"0. Baron Davis" , "0. 3:5" , "0. Only the first stanza" , "0. Nara Muni"]
 second_options = ["1. Stodge" , "1. Sri Lanka" , "1. Three" , "1. Glamorgan" , "1. Wilson Jones" , "1. P.T.Usha" , "1. Northamptonshire & Warwickshire" , "1. 1946" , "1. Mankind" , "1. Ponts" , "1. 25 seconds" , "1. Kevin Garnett" , "1. 2:3" , "1. The whole song" , "1. Bharat Muni"]
 third_options = ["2. Helium Bat" , "2. Zimbabwe" , "2. Four" , "2. Gloucestershire" , "2. Michael Ferreira" , "2. Kamaljit Sandhu" , "2. Nottinghamshire & Worcestershire" , "2. 1956" , "2. Dude Love" , "2. Ponter" , "2. 1 minute" , "2. Stephon Maurbury" , "2. 2:4" , "2. Third and Fourth stanza" , "2. Abhinav Gupt"]
 fourth_options = ["3. Stumpy" , "3. East Africa" , "3. Eight" , "3. Lancashire" , "3. Manoj Kothari" ,"3. K.Malleshwari" , "3. Nottinghamshire & Warwickshire" , "3. 1966" , "3. Mick Foley" , "3. Punter" , "3. 2 minutes" , "3. Reggie Miller" , "3. 3:4" , "3. First and Second stanza", "3. Tandu Muni"]
+
+#another list which contains all the other list that are mentioned above
 all_options = [first_options , second_options , third_options , fourth_options]
 correct_answers = 0
-ans_key = [3 , 1 , 2 , 1 , 1 , 3 , 0 , 1 , 2 , 3 , 0 , 3 , 1 , 0, 1]
+ans_key = [3 , 1 , 2 , 1 , 1 , 3 , 0 , 1 , 2 , 3 , 0 , 3 , 1 , 0, 1] # this list contains all the correct of answers of all 15 questions respectively as a index number
+
 winer_price = [1000,2000,3000,5000,10000,20000,40000,80000,160000,320000,640000,1250000,250000,5000000,10000000]
-# print ans_key
+
 answers_list = []
-listlen = len(questions)
 i = 0
 sum = 0
-while i < listlen:
-      print questions[i] ,"-", len(questions[i])
+while i < len(questions):
+      print questions[i]  #printing a question
       print first_options[i]
       print second_options[i]
       print third_options[i]
       print fourth_options[i]
-      ans = (raw_input("Enter correct answers :- "))
-      ans = int(ans)
-      answers_list.append(ans)
-      if ans_key[i] == ans:
+      ans = int(raw_input("Enter correct answers in the form 0,1,2,3 :- ")) #asking the user for the answers among the options
+      answers_list.append(ans) # inserting our answers into another list
+      if ans_key[i] == ans: # if our ansers is correct then it will do the following things mentioned inside the if statements
       	 correct_answers = correct_answers + 1
          print "Congrats! Aapka answer sahi " , winer_price[i] , "winer price aapko mila hai" , correct_answers , "answer sahi hey aapka"
          sum = winer_price[i] 
          
          if i == 4:
-          	 print "Congrats! Aapka padaav pura ho gaya hai."
+          	 print "Congrats! Aapka  phela padaav pura ho gaya hai."
          elif i == 9:
           		print "Congrats! Aapka doosra padaav pura ho gaya hai."
          elif i == 14:
           		print "Congrats! Aap ek crore rupye jeet gaye hai." 
-      else:
+      else:# if user's answer is not correct then loop will break and game will be stoped with the message that how much cash prize does a player has won .
 	      print "Aap KBC haar chuke hai" , sum , "winer price aapko mila hai"
 	      break   
       i = i + 1
